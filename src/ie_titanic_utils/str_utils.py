5ed08858eabbdcfaeae91cbd26400c430fc1a3a6
maria-aguilera <maria.aguilera@student.ie.edu>
2022-06-23 12:27:15 +0000

    Declare black as development dependancy

--- src/ie_titanic_utils/str_utils.py
+++ src/ie_titanic_utils/str_utils.py
@@ -1,10 +1,9 @@
 import pandas as pd
 
-def extract_title(name,separator= "."):
+
+def extract_title(name, separator="."):
     if not separator:
-        raise ValueError("separator cannot be empty")
+        raise ValueError("Separator cannot be empty")
     fragments = name.split(separator)
     title = fragments[0]
     return title
-
-
